a655f6281af341a07e7a865b0baa6f18d0cf2b85
Ray725 <Ray725@users.noreply.github.com>
2017-09-26 22:02:05 -0400

    get the raw html for course links from each professor page'

--- webscrape.py
+++ webscrape.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-prof_url_array = []
-webpage_text_array = []
-for i in range(10):
-    prof_url_array.append('http://culpa.info/professors/{}'.format(i))
+raw_prof_url_array = []
+num_professors_to_get = 10
 
-for i in range(10):
-    r = requests.get(prof_url_array[i])
+def process_only_courses(input_array):
+    only_courses_array = []
+    for j in range(0, len(input_array)):
+        if 'courses' in str(input_array[j]):
+            only_courses_array.append(input_array[j])
+    return only_courses_array
+
+# generate an array of hypothetical professor urls
+for i in range(num_professors_to_get):
+    raw_prof_url_array.append('http://culpa.info/professors/{}'.format(i))
+
+
+# get raw html links of professor courses
+for i in range(num_professors_to_get):
+    r = requests.get(raw_prof_url_array[i])
     soup = BeautifulSoup(r.text, 'html.parser')
     # return 1 if unfound professor error
     if(len(soup.select('p.error')) == 0):
-        # print(list(set(soup.findAll("span", {"class" : "course_number"}))))
         raw_course_array = soup.select('div.box > p > a')
-        only_courses_array = []
-        for i in range(0, len(raw_course_array)):
-            if 'courses' in str(raw_course_array[i]):
-                only_courses_array.append(raw_course_array[i])
+        # get raw html list of course links
+        only_courses_array = process_only_courses(raw_course_array)
         print(only_courses_array)
