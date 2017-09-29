94e457783de08c2b66b9b69f9a30475c3e3bdc7f
Ray725 <Ray725@users.noreply.github.com>
2017-09-29 16:51:50 -0400

    made an exception case for badly coded course description pages

--- webscrape.py
+++ webscrape.py
@@ -4,7 +4,7 @@ import json
 from bs4 import BeautifulSoup
 
 raw_prof_url_array = []
-num_professors_to_get = 10
+num_professors_to_get = 
 dictionary = {}
 
 
@@ -37,20 +37,24 @@ for i in range(num_professors_to_get):
         only_courses_array = process_only_courses(raw_course_array)
         # get each course link
         course_links = []
-        for i in range(len(only_courses_array)):
+        for j in range(len(only_courses_array)):
             # get course links in an array
             course_links.append(
-                'http://culpa.info{}'.format(only_courses_array[i].get('href')))
+                'http://culpa.info{}'.format(only_courses_array[j].get('href')))
         # now make an array of course numbers from scraping course links
         course_numbers = []
-        for j in range(len(course_links)):
-            q = requests.get(course_links[j])
+        for k in range(len(course_links)):
+            q = requests.get(course_links[k])
             soupTwo = BeautifulSoup(q.text, 'html.parser')
-            course_numbers.append(soupTwo.span.text)
+            try:
+                course_numbers.append(soupTwo.span.text)
+            except:
+                course_numbers.append(soupTwo.h1.text.strip(' \n'))
         # merge with dictionary
         dic_item = {name: course_numbers}
         # make dictionary of professors and corresponding course list
         dictionary.update(dic_item)
+    print("Finished", i)
 
 # export as json
 json = json.dumps(dictionary)
