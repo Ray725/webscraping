70aefbf4bfe1af21b8b5d8f6b1cb0e1e33d0e5c9
Ray725 <Ray725@users.noreply.github.com>
2017-10-10 21:07:55 -0400

    successfully combine JSON files

--- data/make_prof_table.js
+++ data/make_prof_table.js
@@ -5,25 +5,41 @@ var test2 = require('./test2.json');
 var fs = require('fs');
 
 var combined_json = [];
-// iterate through coursesbyprofjson and filter professor JSON and add to a new JSON
-for(var i = 0; i < coursesbyprofjson.length; i++) {
+// iterate coursesbyprofjson and filter professors to add to a new JSON
+for (var i = 0; i < coursesbyprofjson.length; i++) {
   var professor_name = coursesbyprofjson[i].Professor;
-  console.log(professor_name);
-  var professor_json = nuggetsjson.filter(function(value) {
-    if(value.Professor === professor_name) {
-      return value;
+
+  var professor_json;
+  // returns the JSON of matching professor
+  for (var j = 0; j < nuggetsjson.length; j++) {
+    if (nuggetsjson[j].Professor !== undefined) {
+      if (nuggetsjson[j].Professor.toLowerCase() === professor_name.toLowerCase()) {
+        professor_json = nuggetsjson[j];
+        break;
+      }
     }
-  });
-  console.log(professor_json);
-  combined_json.push({"Professor": professor_name, "Nugget": professor_json.Nugget, "Courses": coursesbyprofjson[i].Courses});
+  }
+
+  if (professor_json !== undefined) {
+    if(professor_json.Nugget !== undefined) {
+      combined_json.push({
+        "Professor": professor_name,
+        "Nugget": professor_json.Nugget,
+        "Courses": coursesbyprofjson[i].Courses
+      });
+    }
+  } else {
+    combined_json.push({
+      "Professor": professor_name,
+      "Nugget": "none",
+      "Courses": coursesbyprofjson[i].Courses
+    });
+  }
+
+  professor_json = {};
 }
 
-//console.log(combined_json);
+// successfully combine JSONs
+console.log(combined_json);
 
-fs.writeFile("./combined_json.txt", combined_json, function(err) {
-  if(err) {
-    console.log(err);
-    return;
-  };
-  console.log("File has been created");
-});
+// try to append to a text file line by line
